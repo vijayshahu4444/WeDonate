901607e5a824c167cc0d24951a2f2351d57092c9
superuser6 <imsuperuser6@gmail.com>
2021-03-05 23:06:59 +0530

    wedonate

---
+++ app/src/main/java/com/app/wedonate/demo_activity.java
@@ -0,0 +1,14 @@
+package com.app.wedonate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class demo_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activity);
+    }
+}
