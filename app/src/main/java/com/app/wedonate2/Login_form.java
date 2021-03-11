6ec2f080b8062d1f1411c6c5a5dec3b1d8e301f7
superuser6 <imsuperuser6@gmail.com>
2021-03-11 13:33:04 +0530

    wedonate

--- app/src/main/java/com/app/wedonate2/Login_form.java
+++ app/src/main/java/com/app/wedonate2/Login_form.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class demo_activity extends AppCompatActivity {
+public class Login_form extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_demo_activity);
+        setContentView(R.layout.activity_login_form);
     }
 }
