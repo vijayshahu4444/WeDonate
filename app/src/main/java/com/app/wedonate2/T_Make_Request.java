a9cfdb457282ac2d0566a553622f249a2e39d3d8
Tanvi <tanvikumkar04@gmail.com>
2021-03-16 22:23:37 +0530

    Tanvi

---
+++ app/src/main/java/com/app/wedonate2/T_Make_Request.java
@@ -0,0 +1,15 @@
+package com.app.wedonate2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class T_Make_Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make__request);
+
+    }
+}
