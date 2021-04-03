216942f5c099ad4b16d8f173651a74847368b85a
superuser6 <imsuperuser6@gmail.com>
2021-04-03 19:13:28 +0530

    wedonate

--- app/src/main/java/com/app/wedonate2/Home_Page.java
+++ app/src/main/java/com/app/wedonate2/Home_Page.java
@@ -1,26 +1,59 @@
 package com.app.wedonate2;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class Home_Page extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Home_Page extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     String phone;
     String phone2;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
 
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__page);
         Button button = findViewById(R.id.make_request_button);
-        Toolbar toolbar =   findViewById(R.id.toolbar);
+
+
+
+
+
+        //Hook
+        drawerLayout =findViewById(R.id.drawer_layout);
+        navigationView  = findViewById(R.id.nav_view);
+        toolbar =   findViewById(R.id.toolbar);
+
+        //Tool Bar
+        setSupportActionBar(toolbar);
+
+        //Navigation Drow menu
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //to make menu clickable
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         if(getIntent().getStringExtra("mobile")!= null){
              phone = getIntent().getStringExtra("mobile");
@@ -57,4 +90,22 @@ public class Home_Page extends AppCompatActivity {
             }
         });
     }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
+
+
+
