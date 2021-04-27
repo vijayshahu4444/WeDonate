d13da719105f003f8398830f97170419ebdd4989
superuser6 <imsuperuser6@gmail.com>
2021-04-28 00:51:50 +0530

    wedonate

--- app/src/main/java/com/app/wedonate2/Chat.java
+++ app/src/main/java/com/app/wedonate2/Chat.java
@@ -1,54 +1,116 @@
 package com.app.wedonate2;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Chat extends AppCompatActivity {
-    FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
+    ListView chatList;
+    ArrayList<String > listOfDiscussion = new ArrayList<String>();
+    ArrayAdapter arrayAdapter;
+
+    String UserName;
+    DatabaseReference dbr = FirebaseDatabase.getInstance().getReference("Chat");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, new ChatFragment());
-        fragmentTransaction.commit();
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
+
+        chatList = findViewById(R.id.chatlist);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfDiscussion);
+        chatList.setAdapter(arrayAdapter);
+        getUserName();
+
+        dbr.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                Set<String> set = new HashSet<String>();
+                Iterator i = snapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+
+                }
+                arrayAdapter.clear();
+                arrayAdapter.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+        chatList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),DiscussionActivity.class);
+                intent.putExtra("selected_topic",((TextView)view).getText().toString());
+                intent.putExtra("user_name", UserName);
+                startActivity(intent);
+            }
+        });
+
+
 
 
     }
 
+    private void getUserName(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        EditText userName = new EditText(this);
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListner = new
-            BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        builder.setView(userName);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                UserName = userName.getText().toString();
 
-                    Fragment selectedFragment = null;
 
-                    switch (item.getItemId()) {
-                        case R.id.nav_bottom_chat:
-                            selectedFragment = new ChatFragment();
-                            break;
-                        case R.id.nav_bottom_new_chat:
-                            selectedFragment = new NewMsgFragment();
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+        });
 
-                    return true;
-                }
-            };
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                getUserName();
+
+            }
+        });
+        builder.show();
+    }
+
+
+
 
 }
